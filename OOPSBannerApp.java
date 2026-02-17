7b55f063670c0d43f42a82f5dbb7b957d02f45f5
Navdeep Rathee <navdeeprathee12@gmail.com>
2026-02-17 10:45:30 +0530

    Feature UC3 Completed Successfully

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -4,12 +4,15 @@ class OOPSBannerApp
     public static void main(String[] args)
     {
         Scanner sc=new Scanner(System.in);
-        System.out.println(" ***** "+" "+" ***** "+" "+"****** "+" "+" ***** ");
-        System.out.println("*     *"+" "+"*     *"+" "+"*     *"+" "+"*     *");
-        System.out.println("*     *"+" "+"*     *"+" "+"*     *"+" "+"*      ");
-        System.out.println("*     *"+" "+"*     *"+" "+"****** "+" "+" ***** ");
-        System.out.println("*     *"+" "+"*     *"+" "+"*      "+" "+"      *");
-        System.out.println("*     *"+" "+"*     *"+" "+"*      "+" "+"      *");
-        System.out.println(" ***** "+" "+" ***** "+" "+"*      "+" "+"****** ");
+        System.out.println(String.join("   ",
+                " ***** ", " ***** ", " ***** ", " ***** "));
+        System.out.println(String.join("   ",
+                "*     *", "*     *", "*     *", "*      "));
+        System.out.println(String.join("   ",
+                "*     *", "*     *", " ***** ", " ***** "));
+        System.out.println(String.join("   ",
+                "*     *", "*     *", "*      ", "      *"));
+        System.out.println(String.join("   ",
+                " ***** ", " ***** ", "*      ", " ***** "));
     }
 }
