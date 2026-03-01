92b9f2016bb1031274bfd91beff5961249cc8029
Navdeep Rathee <navdeeprathee12@gmail.com>
2026-03-01 11:07:23 +0530

    Feature UC6 successfully Created

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -3,30 +3,60 @@ class OOPSBannerApp
 {
     public static void main(String[] args)
     {
-        Scanner sc=new Scanner(System.in);
-         String a[] = {
-            String.join("   ", 
-                "  ***  ", "  ***  ", "***** ", " ***** "),
-            String.join("   ", 
-                " *   * ", " *   * ", "*    *", "*      "),
-            String.join("   ", 
-                "*     *", "*     *", "*    *", "*      "),
-            String.join("   ", 
-                "*     *", "*     *", "***** ", " ***** "),
-            String.join("   ", 
-                "*     *", "*     *", "*     ", "      *"),
-            String.join("   ", 
-                "*     *", "*     *", "*     ", "      *"),
-            String.join("   ", 
-                "*     *", "*     *", "*     ", "      *"),
-            String.join("   ", 
-                " *   * ", " *   * ", "*     ", "      *"),
-            String.join("   ", 
-                "  ***  ", "  ***  ", "*     ", " ***** ")
-        };
+        String O1[] = O();
+        String O2[] = O();
+        String P[] = P();
+        String S[] = S();
+        String a[] = new String[9];
+        for (int i = 0; i < 9; i++) 
+        {
+            a[i] = String.join("   ", O1[i], O2[i], P[i], S[i]);
+        }
         for (String i : a) 
         {
             System.out.println(i);
         }
+    }
+    public static String[] O()
+    {
+        return new String[]{
+            "  ***  ",
+            " *   * ",
+            "*     *",
+            "*     *",
+            "*     *",
+            "*     *",
+            "*     *",
+            " *   * ",
+            "  ***  "
+        };
+    }
+    public static String[] P()
+    {
+        return new String[]{
+            "*****  ",
+            "*    * ",
+            "*    * ",
+            "*****  ",
+            "*      ",
+            "*      ",
+            "*      ",
+            "*      ",
+            "*      "
+        };
+    }
+    public static String[] S()
+    {
+        return new String[]{
+            " ***** ",
+            "*      ",
+            "*      ",
+            " ***** ",
+            "      *",
+            "      *",
+            "      *",
+            "      *",
+            " ***** "
+        };
     }
 }
