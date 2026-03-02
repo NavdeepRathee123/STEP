fce1be8573b9e5a53ac98536fa1dc3b0ce34f150
Navdeep Rathee <navdeeprathee12@gmail.com>
2026-03-03 00:28:48 +0530

    Feature UC7 created successfully

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,62 +1,106 @@
 import java.util.*;
-class OOPSBannerApp
+class OOPSBannerApp 
 {
-    public static void main(String[] args)
+    static class CharacterPattern 
     {
-        String O1[] = O();
-        String O2[] = O();
-        String P[] = P();
-        String S[] = S();
-        String a[] = new String[9];
+        char ch;
+        String[] pattern;
+        CharacterPattern(char ch, String[] pattern) 
+        {
+            this.ch = ch;
+            this.pattern = pattern;
+        }
+    }
+    static class CharacterPatternMap 
+    {
+        CharacterPattern[] map = 
+        {
+                new CharacterPattern('O', O()),
+                new CharacterPattern('P', P()),
+                new CharacterPattern('S', S()),
+                new CharacterPattern(' ', space())
+        };
+        String[] get(char c) 
+        {
+            for (CharacterPattern cp : map) 
+            {
+                if (cp.ch == c) return cp.pattern;
+            }
+            return space();
+        }
+    }
+    public static void main(String[] args) 
+    {
+        CharacterPatternMap map = new CharacterPatternMap();
+        String msg = "OOPS";
+        String[] o1 = map.get('O');
+        String[] o2 = map.get('O');
+        String[] p  = map.get('P');
+        String[] s  = map.get('S');
+        String[] finalRows = new String[9];
         for (int i = 0; i < 9; i++) 
         {
-            a[i] = String.join("   ", O1[i], O2[i], P[i], S[i]);
+            finalRows[i] = String.join("   ", o1[i], o2[i], p[i], s[i]);
         }
-        for (String i : a) 
+        for (String row : finalRows) 
         {
-            System.out.println(i);
+            System.out.println(row);
         }
     }
-    public static String[] O()
+    public static String[] O() 
     {
         return new String[]{
-            "  ***  ",
-            " *   * ",
-            "*     *",
-            "*     *",
-            "*     *",
-            "*     *",
-            "*     *",
-            " *   * ",
-            "  ***  "
+                "  ***  ",
+                " *   * ",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                " *   * ",
+                "  ***  "
         };
     }
-    public static String[] P()
+    public static String[] P() 
     {
         return new String[]{
-            "*****  ",
-            "*    * ",
-            "*    * ",
-            "*****  ",
-            "*      ",
-            "*      ",
-            "*      ",
-            "*      ",
-            "*      "
+                "*****  ",
+                "*    * ",
+                "*    * ",
+                "*****  ",
+                "*      ",
+                "*      ",
+                "*      ",
+                "*      ",
+                "*      "
         };
     }
-    public static String[] S()
+    public static String[] S() 
     {
         return new String[]{
-            " ***** ",
-            "*      ",
-            "*      ",
-            " ***** ",
-            "      *",
-            "      *",
-            "      *",
-            "      *",
-            " ***** "
+                " ***** ",
+                "*      ",
+                "*      ",
+                " ***** ",
+                "      *",
+                "      *",
+                "      *",
+                "      *",
+                " ***** "
+        };
+    }
+    public static String[] space() 
+    {
+        return new String[]{
+                "       ",
+                "       ",
+                "       ",
+                "       ",
+                "       ",
+                "       ",
+                "       ",
+                "       ",
+                "       "
         };
     }
 }
